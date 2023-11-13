46695ca8ac7aded16f8454c76c63c27262e4e4e8
Christopher Bohorquez <chrisishere4@outlook.com>
2023-11-13 14:42:23 -0500

    Add files via upload

--- Chris/diabetes_server.py
+++ Chris/diabetes_server.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI, Form
-# from language_model import language_model
+from fastapi.middleware.cors import CORSMiddleware
+from language_model import language_model
+from pydantic import BaseModel
 
 app = FastAPI(title="Diabetes Prediction")
 
+origins = [
+    "http://0.0.0.0:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class TextRequest(BaseModel):
+    text: str
+
+@app.route("/process-text", methods=["OPTIONS"])
+async def options_process_text():
+    return {"methods": ["POST"]}
+
 @app.post("/process-text")
-async def process_text(text: str = Form("default test request")):
-    # response = language_model(text)
-    response = "dummy text"
+async def process_text(requestData: TextRequest):
+    text = requestData.text
+    response = language_model(text)
     return {"response": response}
