752fc052fa125fbc191f847817a7ae8d5f0a7ace
Juan Motta <jrmotta119@gmail.com>
2023-11-13 21:33:57 -0500

    Prediction function and further analysis

--- Juan/diabetes_model_analysis/d_model_construct.py
+++ Juan/diabetes_model_analysis/d_model_construct.py
@@ -4,11 +4,11 @@ from sklearn.model_selection import train_test_split
 import pickle
 
 
-df_d = pd.read_csv('./diabetes_model_analysis/archive/diabetes_binary_health_indicators_BRFSS2015.csv')
+df_d = pd.read_csv('./Juan/diabetes_model_analysis/archive/diabetes_binary_health_indicators_BRFSS2015.csv')
 
-df_d.drop(columns=['Income','Education','Age','Sex','PhysHlth','MentHlth','NoDocbcCost',
-                   'AnyHealthcare','HvyAlcoholConsump','Veggies','Fruits','PhysActivity',
-                   'HeartDiseaseorAttack','Stroke','Smoker','CholCheck'])
+df_d = df_d.drop(columns=['Income','Education','Sex','PhysHlth','MentHlth','NoDocbcCost',
+                   'AnyHealthcare','HvyAlcoholConsump','Veggies','Fruits','PhysActivity'
+                   ,'Stroke','Smoker','CholCheck'])
 
 X = df_d.iloc[:,1:]
 y = df_d.iloc[:,0]
@@ -16,7 +16,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.10, rando
 
 clf = RandomForestClassifier(max_depth=5,random_state= 32)
 clf.fit(X_train,y_train)
-
+print(clf.score(X_test,y_test))
 filename = 'model.sav'
 
 model = pickle.dump(clf,open(filename,'wb'))
